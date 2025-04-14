d073bae776f56d01273610d525a3163f52ef4164
Gary DiCiano <garydiciano4@gmail.com>
2025-04-13 22:44:29 -0400

    Improved Touch Controls
    Using the touch position rather than the delta was the right choice

--- Mobile-Brick-Breaker/Assets/Scripts/Player.cs
+++ Mobile-Brick-Breaker/Assets/Scripts/Player.cs
@@ -3,19 +3,18 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // It's actually the look action, but it's a default so I'll change that up later
-    InputAction moveAction;
+    InputAction touchAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Look");
+        touchAction = InputSystem.actions.FindAction("Touch");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector = new Vector2(moveAction.ReadValue<Vector2>().x, 0.0f);
-        transform.Translate(moveVector * Time.deltaTime);
+        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touchAction.ReadValue<Vector2>());
+        transform.position = new Vector2(touchPosition.x, transform.position.y);
     }
 }
