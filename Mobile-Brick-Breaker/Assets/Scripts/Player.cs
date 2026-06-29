9b5f70479bf725ed8a83183a3cd9c267f6cb6bfb
Gary DiCiano <garydiciano4@gmail.com>
2026-06-29 17:21:41 -0400

    Updated input code
    Now the paddle will only be dragged if the player is currently touching it, rather than moving based on more arbitrarily-placed drags.

--- Mobile-Brick-Breaker/Assets/Scripts/Player.cs
+++ Mobile-Brick-Breaker/Assets/Scripts/Player.cs
@@ -3,32 +3,85 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    InputAction touchAction;
+    InputAction pressAction, positionAction;
+
+    [SerializeField] private Vector2 pointerPosition;
 
     [SerializeField] Powerup.PowerUpType currentPowerUp;
+
+    public bool isSelected;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        touchAction = InputSystem.actions.FindAction("Touch");
+        pressAction = InputSystem.actions.FindAction("Press");
+        positionAction = InputSystem.actions.FindAction("Position");
+
         currentPowerUp = Powerup.PowerUpType.None;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touchAction.ReadValue<Vector2>());
-        transform.position = new Vector2(touchPosition.x, transform.position.y);
+
+        isSelected = checkIfPlayerIsSelected();
+
+        if (isSelected)
+        {
+            pointerPosition = positionAction.ReadValue<Vector2>();
+
+            float z = Camera.main.WorldToScreenPoint(transform.position).z;
+            Vector2 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(pointerPosition.x, pointerPosition.y, 0.0f) + new Vector3(0, 0, z));
+
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+        }
+    }
+
+    private bool checkIfPlayerIsSelected()
+    {
+        pointerPosition = positionAction.ReadValue<Vector2>();
+
+        if (pressAction.IsPressed())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(pointerPosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pointerPosition), Vector2.zero);
+
+            if (hit)
+            {
+                // Debug.Log("RaycastHit hit: " + hit.ToString());
+
+                if (hit.transform == transform)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // Debug.Log("RaycastHit hit nothing");
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("I am the player and I have collided with *something*");
+        // Debug.Log("I am the player and I have collided with *something*");
         GameObject objectCollidedWith = collider.gameObject;
 
         if (objectCollidedWith.CompareTag("Power-Up"))
         {
-            Debug.Log("I the player have indeed collided with a power-up");
+            // Debug.Log("I the player have indeed collided with a power-up");
             Powerup powerUp = objectCollidedWith.GetComponent<Powerup>();
             Powerup.PowerUpType powerUpType = powerUp.powerUpType;
             PowerUp(powerUpType);
@@ -40,14 +93,14 @@ public class Player : MonoBehaviour
         switch (powerUpType)
         {
             case Powerup.PowerUpType.Sticky:
-                Debug.Log("I am the player and I just touched a sticky power up");
+                // Debug.Log("I am the player and I just touched a sticky power up");
                 currentPowerUp = powerUpType;
                 break;
             default:
-                Debug.Log("This type hasn't been implemented yet");
+                // Debug.Log("This type hasn't been implemented yet");
                 break;
         }
 
-        Debug.Log("This is the power up I have set now: " + currentPowerUp.ToString());
+        // Debug.Log("This is the power up I have set now: " + currentPowerUp.ToString());
     }
 }
